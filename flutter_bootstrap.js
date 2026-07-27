95bc761bd568d7475fcff888e5feecb45329e2d7
hk2215 <xionggujunfeng@gmail.com>
2026-07-28 00:21:09 +0900

    シフト表/出勤表AI取込の精度改善を反映(書き写し方式・行を追加・max_tokens調整)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "4261697400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "3918711720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
